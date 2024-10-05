4039341b2da5c4d9b01d1e1415bb711e0d5663be
nmik <maximilian.lausch@student.htw-berlin.de>
2024-10-05 14:23:24 +0200

    different p, m, but same result

--- cal/lm_test.py
+++ cal/lm_test.py
@@ -48,38 +48,128 @@ def make_dF(a, b):
 	return np.array(dF)
 
 
-b0 = 0.2
-b1 = 0.8
-mu = 1
 
-z = np.array([4, 0])
-F = make_F(z[0], z[1])
-dF = make_dF(z[0], z[1])
-I = np.eye(2)
+def lm_step(z, mu):
+	I = np.eye(2)
+	F = make_F(z[0], z[1])
+	dF = make_dF(z[0], z[1])
 
-#F = np.vstack((F, np.zeros((2,1))))
-#F = np.hstack((F, np.zeros(2)))
-#dF = np.vstack((dF, mu*I))
+	cond = (dF.T @ F)
+	for c in cond:
+		if(math.isnan(c) or abs(c) < 1e-10):
+			exit()
+	
+	s = -np.linalg.inv((dF.T @ dF + mu*I)) @ dF.T @ F
+	
+	z1 = z + s
+	F1 = make_F(z1[0], z1[1])
+	FdFs = F + dF @ s 
+	
+	snF = (np.linalg.norm(F))**2
+	snF1 = (np.linalg.norm(F1))**2
+	snFdFs = (np.linalg.norm(FdFs))**2
+	
+	p = (snF - snF1) / (snF - snFdFs)
+	
+	return s, p
 
-print(F, "\n")
-print(dF, "\n")
 
 
 
-s = -np.linalg.inv((dF.T @ dF + mu*I)) @ dF.T @ F
+def lm_advance(z, mu, b0, b1):
+	s, p = lm_step(z, mu)
+	
+	#i = 0
+	while(p <= b0):
+		#tmp = z + s
+		#print(f'{i}| p:{p} | mu: {mu} x:{tmp}')
+		#i += 1
 
-print(s)
+		mu = mu * 2
+		s, p = lm_step(z, mu)
 
-z1 = z + s
-F1 = make_F(z1[0], z1[1])
-FdFs = F + dF @ s 
 
-snF = (np.linalg.norm(F))**2
-snF1 = (np.linalg.norm(F1))**2
-snFdFs = (np.linalg.norm(FdFs))**2
+	# now it is certain that either b0 < p < b1 or p >= b1
+	if(p >= b1):
+		mu = mu / 2.0
+	else:
+		if(not(b0 < p < b1)):
+			print(p)
+			assert(b0 < p < b1)
+	
+	return s, p, mu
 
-p = (snF - snF1) / (snF - snFdFs)
 
-print(p)
+def lm_fit(z, mu, b0, b1):
+	
+
+	zp = z
+	i = 0
+	while(1):
+		s, p, mu = lm_advance(z, mu, b0, b1)
+		z += s
+
+		print(f'{i}| p:{p} | mu:{mu} | x1:{z}')
+		i += 1
+
+		if i > 100:
+			break
+	
+	return z
+
+
+#b0 = 0.2
+#b1 = 0.8
+#
+## initial param estimation
+#mu = 1
+#z = [4, 0]
+
+
+z0 = np.array([4.0,0.0])
+mu0 = 1.0 
+zr = lm_fit(z0, mu0, 0.2, 0.8)
+
+print(zr)
+
+#s, p = lm_step(z0, mu0)
+#print(f'p:{p} | mu0:{mu0} | x:{z0 + s}')
+
+
+#z = np.array([4, 0])
+#F = make_F(z[0], z[1])
+#dF = make_dF(z[0], z[1])
+#
+##F = np.vstack((F, np.zeros((2,1))))
+##F = np.hstack((F, np.zeros(2)))
+##dF = np.vstack((dF, mu*I))
+#
+#print(F, "\n")
+#print(dF, "\n")
+#
+#
+#
+#s = -np.linalg.inv((dF.T @ dF + mu*I)) @ dF.T @ F
+#
+#print(s)
+#
+#z1 = z + s
+#F1 = make_F(z1[0], z1[1])
+#FdFs = F + dF @ s 
+#
+#snF = (np.linalg.norm(F))**2
+#snF1 = (np.linalg.norm(F1))**2
+#snFdFs = (np.linalg.norm(FdFs))**2
+#
+#p = (snF - snF1) / (snF - snFdFs)
+#
+#print(p)
+
+
+	
+
+
+
+
 
 
