60cb2c7452ce12ea1fb082c947768d02158bc055
nmik <maximilian.lausch@student.htw-berlin.de>
2024-11-12 22:40:32 +0100

    added outlier filter to ins. Fixed negative gyroscope values.

--- INS/Core/Src/ins.c
+++ INS/Core/Src/ins.c
@@ -34,6 +34,7 @@ static uint32_t mea_ts = 0;
 
 static int mpu6050_ready = 0;
 static int ins_ready = 0;
+static int of_ready = 0;
 
 static float aref_x = 0.0f;
 static float aref_y = 0.0f;
@@ -55,18 +56,92 @@ int _write(int file, char *ptr, int len) {
 	return len;
 }
 
-static int is_spike(struct mpu6050_measurement *mea)
-{
-	if((mea->accel.x > 1.2 || mea->accel.x < -1.2) ||
-	   (mea->accel.y > 1.2 || mea->accel.y < -1.2) ||
-	   (mea->accel.z > 1.2 || mea->accel.z < -1.2) ||
-	   (mea->gyro.x > 0.1 || mea->gyro.x < -0.1) ||
-	   (mea->gyro.y > 0.1 || mea->gyro.y < -0.1) || 
-	   (mea->gyro.z > 0.1 || mea->gyro.z < -0.1)) {
-		return 1;
+#define WIN_SIZE 5
+#define WIN_MID (WIN_SIZE / 2)
+#define NUM_DEV 10
+#define MAGIC_VAL 1.4826f
+
+#define SWAP(a, b, swp)\
+	swp = a;\
+	a = b;\
+	b = swp;
+
+#define SORT_THREE(arr, swp)\
+	if (arr[0] > arr[1]) {\
+		SWAP(arr[0],arr[1], swp)\
+	}\
+	if (arr[1] > arr[2]) {\
+		SWAP(arr[1],arr[2], swp)\
+	}\
+	if (arr[0] > arr[1]) {\
+		SWAP(arr[0],arr[1], swp)\
 	}
 
-	return 0;
+static float median_filter(float a, float b, float c)
+{
+	float tmp, median, mad;
+	static float arr[3];
+	static float adev[3];
+
+	arr[0] = a;
+	arr[1] = b;
+	arr[2] = c;
+
+	SORT_THREE(arr, tmp);
+	median = arr[1];
+
+	adev[0] = fabs(a - median);	
+	adev[1] = fabs(b - median);	
+	adev[2] = fabs(c - median);	
+
+	SORT_THREE(adev, tmp);
+	mad = adev[1];
+
+	float diff = fabs(b - median);
+	float cap = (NUM_DEV * mad * MAGIC_VAL);
+
+	if(diff > cap)
+		return median;
+	return b;
+}
+
+static struct mpu6050_measurement prev, curr, next;
+static void outlier_filter_init(struct mpu6050_measurement mea)
+{
+	static int cnt = 0;
+
+	if(cnt == 0) {
+		prev = mea;
+	}
+
+	if(cnt == 2) {
+		curr = mea;
+	}
+
+	if(cnt == 4) {
+		next = mea;
+		of_ready = 1;
+	}
+
+	cnt += 1;
+}
+
+static void outlier_filter(void)
+{
+	curr.accel.x = median_filter(prev.accel.x, curr.accel.x, next.accel.x);
+	curr.accel.y = median_filter(prev.accel.y, curr.accel.y, next.accel.y);
+	curr.accel.z = median_filter(prev.accel.z, curr.accel.z, next.accel.z);
+
+	curr.gyro.x = median_filter(prev.gyro.x, curr.gyro.x, next.gyro.x);
+	curr.gyro.y = median_filter(prev.gyro.y, curr.gyro.y, next.gyro.y);
+	curr.gyro.z = median_filter(prev.gyro.z, curr.gyro.z, next.gyro.z);
+	
+}
+static void outlier_filter_advance(struct mpu6050_measurement mea)
+{
+	prev = curr;
+	curr = next;
+	next = mea;
 }
 
 static void ins_dyn_init(struct mpu6050_measurement *mea)
@@ -76,8 +151,6 @@ static void ins_dyn_init(struct mpu6050_measurement *mea)
 	/* obtain avg of accel (for kf) */
 	/* obtain avg of ang vel (for gyro bias) */
 	if(cnt < 5000) {
-		if(is_spike(mea) == 1)
-			return;
 
 		aref_x += mea->accel.x;
 		aref_y += mea->accel.y;
@@ -217,6 +290,30 @@ static void ins_setup(void)
 	mpu6050_ready = 1;
 }
 
+//float maxx, minx;
+//float maxy, miny;
+//float maxz, minz;
+//
+//static void get_minmax(struct mpu6050_measurement *mea)
+//{
+//	maxx = mea->accel.x > maxx ? mea->accel.x : maxx;
+//	maxy = mea->accel.y > maxy ? mea->accel.y : maxy;
+//	maxz = mea->accel.z > maxz ? mea->accel.z : maxz;
+//
+//	minx = mea->accel.x < minx ? mea->accel.x : minx;
+//	miny = mea->accel.y < miny ? mea->accel.y : miny;
+//	minz = mea->accel.z < minz ? mea->accel.z : minz;
+//}
+//
+//static void get_minmax2(struct mpu6050_measurement *mea)
+//{
+//	maxx = mea->gyro.x > maxx ? mea->gyro.x : maxx;
+//	maxy = mea->gyro.y > maxy ? mea->gyro.y : maxy;
+//	maxz = mea->gyro.z > maxz ? mea->gyro.z : maxz;
+//	minx = mea->gyro.x < minx ? mea->gyro.x : minx;
+//	miny = mea->gyro.y < miny ? mea->gyro.y : miny;
+//	minz = mea->gyro.z < minz ? mea->gyro.z : minz;
+//}
 
 void ins_run(void)
 {
@@ -231,23 +328,38 @@ void ins_run(void)
 			assert(i < ARR_SIZE(mea_buf));
 
 			struct mpu6050_measurement mea = mpu6050_decode_raw(mea_buf[i]);
-			
-			if(ins_ready == 0) {
-				ins_dyn_init(&mea);
+
+			//get_minmax2(&mea);
+
+			if(of_ready == 0) {
+				outlier_filter_init(mea);
+			}
+
+			else if(ins_ready == 0) {
+				outlier_filter();
+
+				ins_dyn_init(&curr);
+
+				outlier_filter_advance(mea);
 			}
 			else {
+				outlier_filter();
+
 				int err = kf_filt(kf, 
-					mea.gyro.x, mea.gyro.y, mea.gyro.z,
-					mea.accel.x, mea.accel.y, mea.accel.z);
+					-curr.gyro.x, -curr.gyro.y, -curr.gyro.z,
+					curr.accel.x, curr.accel.y, curr.accel.z);
 
 				assert(err == 0);
 
 				tx_rate_cnt += 1;
-				if(tx_rate_cnt == 100) {
-					transmit_measurements(kf->q, mea);
+				if(tx_rate_cnt == 2) {
+					transmit_measurements(kf->q, curr);
 					tx_rate_cnt = 0;
 				}
+
+				outlier_filter_advance(mea);
 			}
+
 
 			/* ringbuffer */
 			i += 1;
